882a76f14a0aa7808365db65b9338ee04d2edc71
Irsandhabakti <irsandhabakti10@gmail.com>
2016-04-14 06:20:25 +0700

    Added files via upload

---
+++ List Daftar Hadir.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="1019" height="769" border="1" align="center">
+  <tr>
+    <td width="1027" height="177" background="new/header.png" bgcolor="#F0F0F0">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="1027" height="534" background="new/layout 1.png"><p>&nbsp;</p>
+      <form id="form2" name="form2" method="post" action="">
+        <label></label>
+        <div align="center">
+          <select name="select" size="1">
+          </select>
+        </div>
+      </form>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <table width="139" border="1">
+        <tr>
+          <td width="129"><form id="form1" name="form1" method="post" action="">
+            <label>
+              <input type="submit" name="Submit" value="Verifikasi Undangan" />
+              </label>
+          </form>          </td>
+        </tr>
+      </table>
+      <table width="102" border="1">
+        <tr>
+          <td width="92"><input type="submit" name="Submit2" value="History Rapat" /></td>
+        </tr>
+      </table>
+      <table width="129" border="1">
+        <tr>
+          <td width="119"><form id="form4" name="form4" method="post" action="">
+            <label>
+              <input type="submit" name="Submit3" value="List Peserta Rapat" />
+              </label>
+          </form>          </td>
+        </tr>
+      </table>    
+      <div align="center"></div></td>
+  </tr>
+  <tr>
+    <td width="1027" height="23">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="23">&nbsp;</td>
+  </tr>
+</table>
+<div align="center"></div>
+</body>
+</html>
