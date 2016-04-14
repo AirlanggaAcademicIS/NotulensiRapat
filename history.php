e010eddbbde24d0096522d8a4d0c9aedbb0f2ab9
putridk <kputridwi@gmail.com>
2016-04-14 10:49:00 +0700

    Added files via upload

---
+++ history.php
@@ -0,0 +1,21 @@
+<?php
+   $conn =mysql_connect("localhost","root","");
+   $db = mysql_select_db("rapat.sql",$conn);
+   $sql = "select * from table hisori order by tanggalrapat";
+   $hasil = mysql_query($sql,$conn);
+?>
+<h1 align="center" >Histori Rapat</h1>
+<br>
+<table width="656" border="1" cellspacing="0" cellpadding="2">
+   <tr>
+      <td width="107">Judul</td>
+      <td width="212">Tanggal</td>
+   </tr>
+<?php
+   while ($rsbuku = mysql_fetch_row($hasil)){
+   <tr>
+      <td><?php echo $rsbuku[judulrapat]; ?></td>
+      <td><?php echo $rsbuku[tanggalrapat]; ?></td>
+   </tr>
+<?php } ?>
+</table>
