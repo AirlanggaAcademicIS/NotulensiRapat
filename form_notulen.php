f5cc7459cb2daf296c0b4b2b9e61475ad84f6c7d
Rihan Desy Fafi Ruli <rihandfr@gmail.com>
2016-04-29 01:57:37 -0700

    edit form notulen

--- form_notulen.php
+++ form_notulen.php
@@ -1,7 +1,46 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
+<link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="datepicker/jquery-ui.css" />
+<link rel="stylesheet" type="text/css" href="timepicker/jquery.timepicker.min.css" />
+<script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
+<script type="text/javascript" src="combo_bertingkat/jquery.js"></script>
+<script src="datepicker/jquery-ui.js"></script>
+<script type="text/javascript" src="timepicker/jquery.timepicker.min.js"></script>
 <script src='lib/ckeditor/ckeditor.js'></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#haritgl').datepicker({
+			dateFormat : "dd/mm/yy",
+			changeMonth : true,
+			changeYear : true
+			});
+		$('#waktu_rapat').timepicker({ timeFormat: 'HH:mm', interval: 15 });
+		$("#penanggungjawab").change(function(){
+		var penanggungjawab = $("#penanggungjawab").val();
+		$.ajax({
+		url: "combo_bertingkat/ambilnotulen.php",
+		data: "penanggungjawab="+penanggungjawab,
+		cache: false,
+		success: function(msg){
+		$("#notulen").html(msg);
+		}
+		});
+		});
+		$("#notulen").change(function(){
+		var penanggungjawab = $("#penanggungjawab").val();
+		var notulen = $("#notulen").val();
+		$.ajax({
+		url: "combo_bertingkat/ambilpeserta.php",
+		data: {penanggungjawab,notulen},
+		cache: false,
+		success: function(msg){
+		$("#psrapat").html(msg);
+		}
+		});
+		});
+		});
   
   <script type="text/javascript">
     <!-- 
@@ -41,6 +80,7 @@
 	font-size:20px;
 	background-color: rgba(255, 255, 255, 0.5);
   }
+  select:invalid { color: #6B7373; }
 </style>
 <br><br/>
 <form name="form1" id="form1" method="post" action="" onsubmit="return validator()">
@@ -51,7 +91,7 @@
     <td width="700"><table width="100%" >
       <tr>
         <td>
-          <input name="pimpinrapat" type="text" placeholder="Pimpinan Rapat" size="30" /><br 	/>
+          <input name="pimpinrapat" type="text" placeholder="Pimpinan Rapat" size="30"  oninvalid="this.setCustomValidity('Isikan Pimpinan Rapat')" oninput="setCustomValidity('')" required /><br 	/>
         </td>
       </tr>
       <tr>
@@ -59,7 +99,7 @@
       </tr>
       <tr>
         <td>
-          <input name="haritgl" type="text" placeholder="Hari, Tanggal" size="30" />
+          <input name="haritgl" type="text" placeholder="Hari, Tanggal" size="30" oninvalid="this.setCustomValidity('Isikan Tanggaal Rapat')" oninput="setCustomValidity('')" required  />
         </td>
       </tr>
       <tr>
@@ -67,14 +107,14 @@
       </tr>
       <tr>
         <td>
-          <input name="tempatrapat" type="text" placeholder="Tempat Rapat" size="30" />
+          <input name="tempatrapat" type="text" placeholder="Tempat Rapat" size="30" id="haritgl" oninvalid="this.setCustomValidity('Isikan Temapat Rapat')" oninput="setCustomValidity('')" required  />
         </td>
       </tr>
     </table></td>
     <td width="605"><table width="100%" >
       <tr>
         <td>
-          <input name="wakturapat" type="text" placeholder="Waktu Rapat" size="30" />
+          <input name="wakturapat" type="text" placeholder="Waktu Rapat" size="30" oninvalid="this.setCustomValidity('Isikan Waktu Rapat')" oninput="setCustomValidity('')" required  />
         </td>
       </tr>
       <tr>
@@ -82,7 +122,7 @@
       </tr>
       <tr>
         <td>
-          <input name="penanggungjawab" type="text" placeholder="Penanggung Jawab" size="30" />
+          <input name="penanggungjawab" type="text" placeholder="Penanggung Jawab" size="30" oninvalid="this.setCustomValidity('Isikan Penanggung Jawaba Rapat')" oninput="setCustomValidity('')" required  />
         </td>
       </tr>
       <tr>
@@ -90,7 +130,7 @@
       </tr>
       <tr>
         <td>
-          <input name="hadirpeserta" type="text" placeholder="Kehadiran Peserta" size="30" />
+          <input name="hadirpeserta" type="text" placeholder="Kehadiran Peserta" size="30" oninvalid="this.setCustomValidity('Isikan Kehadiran Peserta')" oninput="setCustomValidity('')" required  />
         </td>
       </tr>
     </table></td>
