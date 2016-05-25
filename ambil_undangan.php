18b1dd8cb12e43a9ffb0d7475e62a9373ff6b30e
ArdhiJayaPerdana <Ardhijp76@gmail.com>
2016-05-25 17:59:43 +0700

    Add files via upload
    revisi ambil undangan
    
    to do : memperbagus tampilan dan sambungan ke database
    do : menambahkan efek dari css
    hambatan : saat dicoba pada komputer dan saat dicoba dari data github hasilnya berbeda

--- ambil_undangan.php
+++ ambil_undangan.php
@@ -13,52 +13,167 @@ if($_SESSION[username]==''){
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+<meta charset="utf-8">
+<link href="lib/bootstrap.min.css" rel="stylesheet">
+<link href="lib/buttons.css" rel="stylesheet">
+<link rel="apple-touch-icon" href="/bootstrap/img/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/bootstrap/img/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/bootstrap/img/apple-touch-icon-114x114.png">
+<script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<style type="text/css">
+.table-responsive {height:460px;}
+</style>
 <style type="text/css">
 <!--
 .style1 {font-family: Arial, Helvetica, sans-serif}
 -->
 </style>
 </head>
-
 <body>
-<br><br/>
+<br><br/> 
+ <div class="container">
+	<div class="row">
+      <div class="table-responsive">
+        <table class="table table-hover">
+          <tbody id="myTable">
 <?php
-$r = mysql_query("SELECT * FROM rapat");
-
+$r = mysql_query("SELECT * FROM rapat where status='0'");
 while($hasil = mysql_fetch_array($r)){
 	?>
-	<table width="100%"  border="1" >
-  <tr>
-    <td>
-      <table width="100%"  border="0">
-         <tr>
-           <td width="64%"><table width="100%"  border="0">
-		   
-             <tr>
-               <td><span class="style1">Nomor Surat : <?php echo $hasil['no_surat'];?></span></td>
-             </tr>
-             <tr>
-               <td><span class="style1">Judul Pembahasan : <?php echo $hasil['pembahasan_rapat'];?></span></td>
-             </tr>
-             <tr>
-               <td><span class="style1">Waktu Rapat : <?php echo $hasil['tanggal'];?></span></td>
-             </tr>
-			 <tr>
-               <td><span class="style1">Tempat : <?php echo $hasil['tempat'];?></span></td>
-			 </tr>
-           </table></td>
-		   
-		   <td>
-		   <img src= width="500" height="100">
-		   </td>
-           <td width="36%" align="center" valign="middle"><a href='hal_kaprodi.php?konf=<?php echo $hasil['no_surat'];?>'><img src="image/konfirmasi_button.png" width="100" height="32"></a></td>
-         </tr>
-       </table>
-    </td>
-  </tr>
+	<tr>
+	<td width="65%"><p>Nomor Surat : <?php echo $hasil['no_surat'];?></p>
+	<p>Judul Pembahasan : <?php echo $hasil['pembahasan_rapat'];?></p>
+	<p>Waktu Rapat : <?php echo $hasil['tanggal'];?></p>
+	<p>Tempat : <?php echo $hasil['tempat'];?></p>
+	
+	</td>
+	
+	<td width="75%"  align="center" valign="middle"><div align="center"><p><a href='hal_kaprodi.php?konf=<?php echo $hasil['no_surat'];?>'><img src="image/konfirmasi_button.png" width="100" height="32"></a></div></p></td>
+	
+	
+</tbody>
 </table>	
 <?php   
 }
 ?>
+
+</div>
+      <div class="col-md-12 text-center">
+      <ul class="pagination pagination-lg pager" id="myPager"></ul>
+      </div>
+	</div>
+</div>
+<script type='text/javascript'>
+        $.fn.pageMe = function(opts){
+    var $this = this,
+        defaults = {
+            perPage: 7,
+            showPrevNext: false,
+            hidePageNumbers: false
+        },
+        settings = $.extend(defaults, opts);
+    
+    var listElement = $this;
+    var perPage = settings.perPage; 
+    var children = listElement.children();
+    var pager = $('.pager');
+    
+    if (typeof settings.childSelector!="undefined") {
+        children = listElement.find(settings.childSelector);
+    }
+    
+    if (typeof settings.pagerSelector!="undefined") {
+        pager = $(settings.pagerSelector);
+    }
+    
+    var numItems = children.size();
+    var numPages = Math.ceil(numItems/perPage);
+
+    pager.data("curr",0);
+    
+    if (settings.showPrevNext){
+        $('<li><a href="#" class="prev_link"><<</a></li>').appendTo(pager);
+    }
+    
+    var curr = 0;
+    while(numPages > curr && (settings.hidePageNumbers==false)){
+        $('<li><a href="#" class="page_link">'+(curr+1)+'</a></li>').appendTo(pager);
+        curr++;
+    }
+    
+    if (settings.showPrevNext){
+        $('<li><a href="#" class="next_link">>></a></li>').appendTo(pager);
+    }
+    
+    pager.find('.page_link:first').addClass('active');
+    pager.find('.prev_link').hide();
+    if (numPages<=1) {
+        pager.find('.next_link').hide();
+    }
+  	pager.children().eq(1).addClass("active");
+    
+    children.hide();
+    children.slice(0, perPage).show();
+    
+    pager.find('li .page_link').click(function(){
+        var clickedPage = $(this).html().valueOf()-1;
+        goTo(clickedPage,perPage);
+        return false;
+    });
+    pager.find('li .prev_link').click(function(){
+        previous();
+        return false;
+    });
+    pager.find('li .next_link').click(function(){
+        next();
+        return false;
+    });
+    
+    function previous(){
+        var goToPage = parseInt(pager.data("curr")) - 1;
+        goTo(goToPage);
+    }
+     
+    function next(){
+        goToPage = parseInt(pager.data("curr")) + 1;
+        goTo(goToPage);
+    }
+    
+    function goTo(page){
+        var startAt = page * perPage,
+            endOn = startAt + perPage;
+        
+        children.css('display','none').slice(startAt, endOn).show();
+        
+        if (page>=1) {
+            pager.find('.prev_link').show();
+        }
+        else {
+            pager.find('.prev_link').hide();
+        }
+        
+        if (page<(numPages-1)) {
+            pager.find('.next_link').show();
+        }
+        else {
+            pager.find('.next_link').hide();
+        }
+        
+        pager.data("curr",page);
+      	pager.children().removeClass("active");
+        pager.children().eq(page+1).addClass("active");
+    
+    }
+};
+
+$(document).ready(function(){
+    
+  $('#myTable').pageMe({pagerSelector:'#myPager',showPrevNext:true,hidePageNumbers:false,perPage:4});
+    
+});
+        </script>
+		
+
 </body>
 </html>
