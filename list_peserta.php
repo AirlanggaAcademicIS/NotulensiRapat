538aa43a07d4e6921ae45143b6254b031f792330
Irsandhabakti <irsandhabakti10@gmail.com>
2016-06-01 22:10:23 +0700

    Add files via upload

--- list_peserta.php
+++ list_peserta.php
@@ -4,43 +4,49 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Untitled Document</title>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#cmbGate").change(function(){
+		var ps = $("#cmbGate").val();
+		$.ajax({
+		url: "cetak.php",
+		data: "ps="+ps,
+		cache: false,
+		success: function(msg){
+		$("#list").html(msg);
+		}
+		});
+		});
+		});
+</script>
 </head>
 <body>
+<form action="" method="post" name="Form1">
 <table width="100%" border="0">
   <tr>
-    <td width="37"><img src="image/Graphic1.png" width="36" height="30" /></td>
-    <td width="1056"><form id="form1" name="form1" method="post" action="">
-      <label for="select2"></label>
-      <select
-     <span class="caret"></span></button>
-    <ul class="dropdown-menu">
-  <?php 
+    <td><img src="image/Graphic1.png" width="36" height="30" /></td>
+    <td><select name="cmbGate" id="cmbGate" >
+      <span class="caret"></span>
+      <ul class="dropdown-menu">
+      <?php 
   $result=mysql_query("SELECT * FROM rapat where status ='1'");
   while ($nama = mysql_fetch_array($result)){
 	  echo "<option value=\"$nama[no_surat]\">$nama[hal]</option>\n";
   }
   ?>
-  
-      </select>
-    </form>
-    </td>
+    </select></td>
   </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td><form id="form2" name="form2" method="post" action="">
-    </form>
-    </td>
-  </tr>
+</table>
+<div id="list">
+<table width="100%" border="0">
   <tr>
     <td><img src="image/Tanggal.png" width="35" height="29" /></td>
-    <td><form id="form3" name="form3" method="post" action="">
-    </form>
+    <td>
     </td>
   </tr>
   <tr>
     <td>&nbsp;</td>
-    <td><form id="form4" name="form4" method="post" action="">
-    </form>
+    <td>
     </td>
   </tr>
 </table>
@@ -89,49 +95,51 @@
 </table>
 <table width="100%" border="1">
   <tr>
-    <td><form id="form5" name="form5" method="post" action="">
+    <td>
       <label for="textfield"></label>
       <input type="text" name="textfield" id="textfield" />
-    </form></td>
-    <td><form id="form11" name="form11" method="post" action="">
+    </td>
+    <td>
       <label for="select2"></label>
-    </form></td>
+    </td>
   </tr>
   <tr>
-    <td><form id="form6" name="form6" method="post" action="">
+    <td>
       <label for="textfield2"></label>
       <input type="text" name="textfield5" id="textfield2" />
-    </form></td>
+   </td>
     <td>&nbsp;</td>
   </tr>
   <tr>
-    <td><form id="form7" name="form7" method="post" action="">
+    <td>
       <label for="textfield3"></label>
       <input type="text" name="textfield6" id="textfield3" />
-    </form></td>
+    </td>
     <td>&nbsp;</td>
   </tr>
   <tr>
-    <td><form id="form8" name="form8" method="post" action="">
+    <td>
       <label for="textfield4"></label>
       <input type="text" name="textfield7" id="textfield4" />
-    </form></td>
+    </td>
     <td>&nbsp;</td>
   </tr>
   <tr>
-    <td><form id="form9" name="form9" method="post" action="">
+    <td>
       <label for="textfield5"></label>
       <input type="text" name="textfield8" id="textfield5" />
-    </form></td>
+    </td>
     <td>&nbsp;</td>
   </tr>
   <tr>
-    <td><form id="form10" name="form10" method="post" action="">
+    <td>
       <label for="textfield6"></label>
       <input type="text" name="textfield9" id="textfield6" />
-    </form></td>
+   </td>
     <td>&nbsp;</td>
   </tr>
 </table>
+</div>
+</form>
 </body>
 </html>
